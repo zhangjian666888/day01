26897a2d90f6074ade5a26c7388894357e17b7de
zhangjian666888 <1152765051@qq.com>
2019-08-03 10:08:42 +0800

    添加了以个页面

---
+++ day01/src/view/aaa.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "aaa"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ day01/src/view/s.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "s"
+    }
+</script>
+
+<style scoped>
+
+</style>
